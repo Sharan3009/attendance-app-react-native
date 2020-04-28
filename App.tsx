dfb5a98d1f58521c9b4643a8b2a6325684bd55d7
Sharandeep Singh <Sharandeep.Singh@niceactimize.com>
2020-04-28 22:21:39 +0530

    added date time picker

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import DateTimePicker from '@react-native-community/datetimepicker';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+        <DateTimePicker value={new Date()} mode="time" />
     </View>
   );
 }

a21c4ef997b8411720edc0b234663f45c9dcb8fa
Sharandeep Singh <Sharandeep.Singh@niceactimize.com>
2020-04-29 23:14:56 +0530

    working timestamp in emulator

--- App.js
+++ App.js
@@ -8,18 +8,28 @@
 
 import React from 'react';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { Text } from 'react-native';
 class App extends React.Component{
-  onChange = () =>{
-    //do nothing
+  constructor(props){
+    super(props)
+    this.state={
+      time : null
+    }
+  }
+  onChange = (e) =>{
+    this.setState({time:e.nativeEvent.timestamp})
   }
   render() {
     return(
+      <>
+      <Text>{this.state.time}</Text>
       <DateTimePicker
         value={new Date()}
         mode={"time"}
         display="default"
         onChange={this.onChange}
     />
+    </>
     );
   };
 };

7ec3e47bd9df6fea5eb2ae097c598d383ca5278e
Sharandeep Singh <Sharandeep.Singh@niceactimize.com>
2020-04-29 19:12:29 +0530

    re created

--- android/app/src/main/java/com/attendanceapp/MainActivity.java
+++ android/app/src/main/java/com/attendanceapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.att;
+package com.attendanceapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "att";
+    return "attendanceApp";
   }
 }

436897a0d5f6cc366c6c741dce95d99497a378be
Sharandeep Singh <Sharandeep.Singh@niceactimize.com>
2020-04-30 13:42:05 +0530

    removed datetimepicker

--- App.js
+++ App.js
@@ -7,28 +7,20 @@
  */
 
 import React from 'react';
-import DateTimePicker from '@react-native-community/datetimepicker';
 import { Text } from 'react-native';
 class App extends React.Component{
   constructor(props){
     super(props)
-    this.state={
-      time : null
+    this.state = {
+      text : "Haye ni tera koka"
     }
   }
-  onChange = (e) =>{
-    this.setState({time:e.nativeEvent.timestamp})
-  }
+
   render() {
     return(
       <>
-      <Text>{this.state.time}</Text>
-      <DateTimePicker
-        value={new Date()}
-        mode={"time"}
-        display="default"
-        onChange={this.onChange}
-    />
+      <Text>{this.state.text}</Text>
+      
     </>
     );
   };

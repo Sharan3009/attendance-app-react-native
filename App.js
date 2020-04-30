70f4ab73cc662f636bbfffcd13bf0a9b72b3f056
Sharandeep Singh <Sharandeep.Singh@niceactimize.com>
2020-04-30 14:49:51 +0530

    adding text to storage

--- App.js
+++ App.js
@@ -7,21 +7,54 @@
  */
 
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text,TextInput , Button, AsyncStorage } from 'react-native';
 class App extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      text : "Haye ni tera koka"
+      text : null,
+      finalText:null
     }
+  }
+
+  componentDidMount(){
+    this.getTextFromStorage();
+  }
+
+  onChangeText = (e) =>{
+    this.setState({text:e})
+  }
+
+  saveTextToStorage = () =>{
+    AsyncStorage.setItem("finalText",this.state.text).then(
+      (e)=>{
+        this.setState({finalText:this.state.text,text:null});
+      }
+    )
+  }
+
+  getTextFromStorage = () =>{
+    AsyncStorage.getItem("finalText").then(
+      (e)=>{
+        this.setState({finalText:e})
+      }
+    )
   }
 
   render() {
     return(
-      <>
-      <Text>{this.state.text}</Text>
-      
-    </>
+      <View style={{height:"100%", flexDirection:"column", justifyContent:"center"}}>
+       <View style={{flexDirection:"row"}}>
+        <TextInput 
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, flex:1 }} 
+        onChangeText={this.onChangeText}
+        value={this.state.text}/>
+        <Button title="Save" onPress={this.saveTextToStorage}/>
+      </View>
+      <View style={{alignItems:"center"}}>
+        <Text>{this.state.finalText}</Text>
+      </View>
+    </View>
     );
   };
 };
